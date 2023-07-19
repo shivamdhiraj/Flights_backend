d533987e264f4200763a2b52a819f3401e570572
shivamdhiraj <singh1401shivam@gmail.com>
2023-07-19 19:08:07 +0530

    Added city-middleware

---
+++ src/middlewares/city-middlewares.js
@@ -0,0 +1,21 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { ErrorResponse } = require('../utils/common');
+const AppError = require('../utils/errors/app-error');
+const { log } = require('winston');
+
+function validateCreateRequest(req, res, next) {
+    //console.log(req.body);
+    if(!req.body.name) {
+        ErrorResponse.message = 'Something went wrong while creating city';
+        ErrorResponse.error = new AppError(['city name not found in the incoming request in the correct form'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse);
+    }
+    next();
+}
+
+module.exports = {
+    validateCreateRequest
+}
